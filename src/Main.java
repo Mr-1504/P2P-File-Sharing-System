61bcfa801e53fea5e9c525fa9c95db7398e1b1b9
Mr-1504 <truongvan.minh1504@gmail.com>
2025-05-12 22:56:48 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
+import controller.PeerController;
+import model.PeerModel;
+import view.PeerView;
+
+import javax.swing.*;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SwingUtilities.invokeLater(() -> {
+            try {
+                PeerModel model = new PeerModel("localhost", 5000);
+                PeerView view = new PeerView();
+                PeerController controller = new PeerController(model, view);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+                model.startServer();
+                view.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
