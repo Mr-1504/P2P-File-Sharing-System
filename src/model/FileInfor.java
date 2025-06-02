d2b513f90359d26350930dbd254d37850e662b82
Mr-1504 <truongvan.minh1504@gmail.com>
2025-06-02 14:26:29 +0700

    Update FileInfor.java

--- src/model/FileInfor.java
+++ src/model/FileInfor.java
@@ -1,12 +1,34 @@
 package model;
 
+import java.util.List;
+
 public class FileInfor {
     private String fileName;
+    private long fileSize;
+    private List<String> chunkHashes;
     private PeerInfor peerInfor;
 
-    public FileInfor(String fileName, PeerInfor peerInfor) {
+    public FileInfor(String fileName, long fileSize, List<String> chunkHashes, PeerInfor peerInfor) {
         this.fileName = fileName;
         this.peerInfor = peerInfor;
+        this.fileSize = fileSize;
+        this.chunkHashes = chunkHashes;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public List<String> getChunkHashes() {
+        return chunkHashes;
+    }
+
+    public void setChunkHashes(List<String> chunkHashes) {
+        this.chunkHashes = chunkHashes;
     }
 
     public String getFileName() {
