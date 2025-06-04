b1361d7aa87056134cdf5b0791ad6523946b37ad
Mr-1504 <truongvan.minh1504@gmail.com>
2025-06-04 11:45:02 +0700

    Add discovery peer using UDP

--- src/model/TrackerModel.java
+++ src/model/TrackerModel.java
@@ -4,17 +4,26 @@ import utils.Infor;
 import utils.RequestInfor;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.nio.Buffer;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class TrackerModel {
     private final int TRACKER_PORT = Infor.TRACKER_PORT;
-    private Map<String, List<String>> fileToPeers; // Maps file names to a list of peer IPs
+    private final Map<String, List<String>> fileToPeers;
+    private final Set<String> knownPeers;
+    private final ExecutorService executor;
 
     public TrackerModel() {
         fileToPeers = new HashMap<>();
+        knownPeers = new HashSet<>();
+        executor = java.util.concurrent.Executors.newCachedThreadPool();
+        ScheduledExecutorService pingExecutor = java.util.concurrent.Executors.newScheduledThreadPool(1);
+        pingExecutor.scheduleAtFixedRate(this::pingPeers, 0, 60, java.util.concurrent.TimeUnit.SECONDS);
     }
 
     public void startTracker() {
@@ -35,7 +44,7 @@ public class TrackerModel {
                             String peerIp = parts[1];
                             String peerPort = parts[2];
                             String peerInfor = peerIp + "|" + peerPort;
-                            fileToPeers.computeIfAbsent("peers", k -> new java.util.ArrayList<>()).add(peerIp);
+                            knownPeers.add(peerInfor);
                             out.println(peerInfor);
                         } else {
                             out.println("Invalid REGISTER request format. Use: REGISTER|<peerIp>|<peerPort>");
@@ -75,6 +84,80 @@ public class TrackerModel {
         } catch (
                 Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void pingPeers() {
+        executor.submit(() -> {
+            try (DatagramSocket socket = new DatagramSocket()) {
+                socket.setSoTimeout(10000);
+
+                for (String peer : knownPeers) {
+                    String[] peerInfor = peer.split("\\|");
+                    String peerIp = peerInfor[0];
+                    int peerPort = Integer.parseInt(peerInfor[1]);
+
+                    byte[] pingMessage = RequestInfor.PING.getBytes();
+
+                    DatagramPacket packet = new DatagramPacket(pingMessage, pingMessage.length, InetAddress.getByName(peerIp), peerPort);
+
+                    socket.send(packet);
+                }
+
+                long startTime = System.currentTimeMillis();
+                Set<String> alivePeers = new HashSet<>();
+
+                while (System.currentTimeMillis() - startTime < 10000) {
+                    try {
+                        byte[] buffer = new byte[1024];
+                        DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+                        socket.receive(response);
+
+                        String responseMessage = new String(response.getData(), 0, response.getLength());
+                        if (responseMessage.equals(RequestInfor.PONG)) {
+                            String ip = response.getAddress().getHostAddress();
+                            int port = response.getPort();
+                            String peerInfor = ip + "|" + port;
+
+                            alivePeers.add(peerInfor);
+                            System.out.println("Received PONG from: " + peerInfor);
+                        }
+                    } catch (SocketTimeoutException e) {
+                        break;
+                    } catch (IOException e) {
+                        System.err.println("Error receiving response: " + e.getMessage());
+                    }
+                }
+
+                updateKnownPeers(alivePeers);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void updateKnownPeers(Set<String> alivePeers) {
+        knownPeers.retainAll(alivePeers);
+        for (String peer : alivePeers) {
+            if (!knownPeers.contains(peer)) {
+                knownPeers.add(peer);
+                System.out.println("New peer added: " + peer);
+            }
+        }
+        System.out.println("Known peers updated. Current count: " + knownPeers.size());
+
+        for(List<String> peers : fileToPeers.values()) {
+            for(String peer : peers) {
+                if (!alivePeers.contains(peer)) {
+                    peers.remove(peer);
+                    System.out.println("Removed dead peer: " + peer);
+                }
+                if (peers.isEmpty()) {
+                    fileToPeers.remove(peers);
+                    System.out.println("Removed empty file entry for: " + peers);
+                }
+            }
         }
     }
 }
