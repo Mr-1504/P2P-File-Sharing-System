2dfd8d9e93b966044035db1884644e71cfe8f495
Mr-1504 <truongvan.minh1504@gmail.com>
2025-06-06 12:31:47 +0700

    Add tags

--- src/utils/LogTag.java
+++ src/utils/LogTag.java
@@ -3,8 +3,8 @@ package utils;
 public interface LogTag {
     String ERROR = "ERROR";
     String CANCELLED = "CANCELLED";
-    int NOTFOUND = -1;
     int iERROR = 0;
     int SUCCESS = 1;
-    int NOMORE = 2;
+    int NOT_FOUND = 2;
+    int INVALID = -1;
 }
