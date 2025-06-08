0c94501903f7d54a13867e3763f52a556c88dc4a
Mr-1504 <truongvan.minh1504@gmail.com>
2025-06-09 01:09:54 +0700

    Update RequestInfor.java

--- src/utils/RequestInfor.java
+++ src/utils/RequestInfor.java
@@ -14,4 +14,6 @@ public interface RequestInfor {
     String PING = "PING";
     String SHARE_LIST = "LSHARE";
     String UNSHARED_FILE = "UNSHARED_FILE";
+    String REFRESH = "REFRESH";
+    String REFRESHED = "REFRESHED";
 }
