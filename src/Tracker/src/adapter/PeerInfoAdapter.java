38e013f7b8369a70261ecdc26366ca5aa82fdf77
Trương Văn Minh <truongvan.minh1504@gmail.com>
2025-10-19 13:00:25 +0700

    Add PeerInfoAdapter for Gson serialization
    Introduces PeerInfoAdapter in both Tracker and backend_layer modules to handle custom serialization and deserialization of PeerInfo objects using Gson. This enables PeerInfo to be represented as a colon-separated string for JSON operations.

---
+++ src/Tracker/src/adapter/PeerInfoAdapter.java
@@ -0,0 +1,22 @@
+package src.adapter;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import src.model.PeerInfo;
+
+import java.io.IOException;
+
+public class PeerInfoAdapter extends TypeAdapter<PeerInfo> {
+    @Override
+    public void write(JsonWriter jsonWriter, PeerInfo peerInfo) throws IOException {
+        jsonWriter.value(peerInfo.getIp() + ":" + peerInfo.getPort() + ":" + peerInfo.getUsername());
+    }
+
+    @Override
+    public PeerInfo read(JsonReader jsonReader) throws IOException {
+        String value = jsonReader.nextString();
+        String[] parts = value.split(":");
+        return new PeerInfo(parts[0], Integer.parseInt(parts[1]), parts[2]);
+    }
+}
